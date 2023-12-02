e8d6c42f6dbf11863907b96ef41e7ab13f7b1e1c
Galeid <galeid475@gmail.com>
2023-12-02 17:06:08 -0500

    feat: create app main components & add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,53 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginComponent } from './views/login/login.component';
+import { NotFoundComponent } from './views/not-found/not-found.component';
+import { RegisterComponent } from './views/register/register.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { HomeComponent } from './views/main/home/home.component';
+import { MainLayoutComponent } from './layouts/main/main.layout.component';
+import { PreviewComponent } from './views/main/preview/preview.component';
+import { DashboardLayoutComponent } from './layouts/dashboard/dashboard.layout.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+      },
+      {
+        path: 'preview',
+        component: PreviewComponent,
+      },
+    ],
+  },
+  {
+    path: 'dashboard',
+    component: DashboardLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: DashboardComponent,
+      },
+    ],
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'not-found',
+    component: NotFoundComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '/not-found',
+  },
+];
